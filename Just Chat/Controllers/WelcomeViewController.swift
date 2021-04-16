3865ae95fcc172253773e6ad3809c6312520f770
Li Fengyu <fengyuli2002@gmail.com>
2021-04-16 17:14:19 +0800

    Animate title text

--- Just Chat/Controllers/WelcomeViewController.swift
+++ Just Chat/Controllers/WelcomeViewController.swift
@@ -14,7 +14,19 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Animate the title text
+        titleLabel.text = ""
+        // charIndex used to customize TimeInterval for Timer
+        var charIndex = 0.0
+        let titleText = "⚡️Just Chat"
+        for character in titleText {
+            // Use closure
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(character)
+            }
+            charIndex += 1
+        }
        
     }
     
